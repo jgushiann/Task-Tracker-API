6ff713fd6a0d39c4d653a6fbad62f4a7f4f4d307
jgushiann <njgus21@freeuni.edu.ge>
2025-10-24 14:30:59 +0400

    added repos

---
+++ TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/repository/TaskRepository.java
@@ -0,0 +1,40 @@
+package com.nini.TaskTrackerAPI.repository;
+
+import com.nini.TaskTrackerAPI.model.*;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {
+    Optional<Task> findByTask_id(long id);
+    Optional<Task> findByTitle(String title);
+    Optional<Task> findByDescription(String description);
+    List<Task> findByPriority(Priority priority);
+    List<Task> findByCategory(Category category);
+    List<Task> findByStatus(Status status);
+    List<Task> findByDueDate(LocalDate dueDate);
+    List<Task> findByAssignedUser(User assignedUser);
+
+    List<Task> findByTitleContaining(String title);
+    List<Task> findByDescriptionContaining(String description);
+
+    Boolean existsByTask_id(long task_id);
+    Boolean existsByTitleContaining(String title);
+    Boolean existsByDescriptionContaining(String description);
+    Boolean existsByPriority(Priority priority);
+    Boolean existsByCategory(Category category);
+    Boolean existsByStatus(Status status);
+    Boolean existsByDueDate(LocalDate dueDate);
+    Boolean existsByAssignedUser(User assignedUser);
+
+    void deleteByTask_id(long task_id);
+    void deleteByTitleContaining(String title);
+    void deleteByDescriptionContaining(String description);
+    void deleteByPriority(Priority priority);
+    void deleteByCategory(Category category);
+    void deleteByStatus(Status status);
+    void deleteByDueDate(LocalDate dueDate);
+    void deleteByAssignedUser(User assignedUser);
+}
---
+++ TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/repository/UserRepository.java
@@ -0,0 +1,30 @@
+package com.nini.TaskTrackerAPI.repository;
+
+import com.nini.TaskTrackerAPI.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findByUser_id(long user_id);
+    List<User> findByFirst_name(String first_name);
+    List<User> findByLast_name(String last_name);
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
+
+    List<User> findByFirst_nameContaining(String first_name);
+    List<User> findByLast_nameContaining(String last_name);
+
+    Boolean existsByUser_id(long user_id);
+    Boolean existsByFirst_name(String first_name);
+    Boolean existsByLast_name(String last_name);
+    Boolean existsByEmail(String email);
+    Boolean existsByUsername(String username);
+
+    void deleteByUser_id(long user_id);
+    void deleteByFirst_name(String first_name);
+    void deleteByLast_name(String last_name);
+    void deleteByEmail(String email);
+    void deleteByUsername(String username);
+}
