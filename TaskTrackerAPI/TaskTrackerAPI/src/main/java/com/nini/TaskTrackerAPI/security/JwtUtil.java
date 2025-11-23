7ce4ec719d3286e9a2434d18cf841a1b5101c6b7
jgushiann <njgus21@freeuni.edu.ge>
2025-11-23 23:09:08 +0400

    fix

--- TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/security/JwtUtil.java
+++ TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/security/JwtUtil.java
@@ -2,14 +2,14 @@ package com.nini.TaskTrackerAPI.security;
 
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.security.Keys;
-import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 import java.util.Date;
 
-@Slf4j
+@Component
 public class JwtUtil {
     private static final String SECRET = "mysupersecretkeyformyjwtthatistoolong321";
 
