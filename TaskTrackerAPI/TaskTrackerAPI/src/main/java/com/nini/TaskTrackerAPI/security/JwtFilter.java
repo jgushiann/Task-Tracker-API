1f04bd7c97ea61c8596fc9aaf97e5243dcf8a243
jgushiann <njgus21@freeuni.edu.ge>
2025-11-24 11:07:28 +0400

    added JwtFilter

--- TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/security/JwtFilter.java
+++ TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/security/JwtFilter.java
@@ -1,8 +1,45 @@
 package com.nini.TaskTrackerAPI.security;
+import com.nini.TaskTrackerAPI.service.UserService;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
 
+import java.io.IOException;
 
+@RequiredArgsConstructor
 @Component
-public class JwtFilter {
+public class JwtFilter extends OncePerRequestFilter{
+
+    private final UserService userService;
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public void doFilterInternal(HttpServletRequest req, HttpServletResponse res, FilterChain chain) throws IOException, ServletException {
+        String authorizationHeader = req.getHeader("Authorization");
+        String token = null;
+        String username = null;
+
+        if(authorizationHeader != null && authorizationHeader.startsWith("Bearer ")){
+            token = authorizationHeader.substring(7);
+            username = jwtUtil.extractUsername(token);
+        }
+
+        if(username != null && SecurityContextHolder.getContext().getAuthentication() == null){
+            UserDetails userDetails = userService.loadUserByUsername(username);
+            if(jwtUtil.isTokenValid(token)){
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+            }
+        }
+        chain.doFilter(req, res);
+    }
 
 }
