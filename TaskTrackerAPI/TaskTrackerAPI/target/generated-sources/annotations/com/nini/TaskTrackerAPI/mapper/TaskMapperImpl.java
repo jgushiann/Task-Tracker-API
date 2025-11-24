7ca4f6f86119baac7d0991a8b3bf4482098e26b2
jgushiann <njgus21@freeuni.edu.ge>
2025-11-24 18:26:54 +0400

    fix

---
+++ TaskTrackerAPI/TaskTrackerAPI/target/generated-sources/annotations/com/nini/TaskTrackerAPI/mapper/TaskMapperImpl.java
@@ -0,0 +1,78 @@
+package com.nini.TaskTrackerAPI.mapper;
+
+import com.nini.TaskTrackerAPI.dto.TaskRequestDTO;
+import com.nini.TaskTrackerAPI.dto.TaskResponseDTO;
+import com.nini.TaskTrackerAPI.model.Task;
+import com.nini.TaskTrackerAPI.model.User;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-11-24T18:12:04+0400",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.16 (Eclipse Adoptium)"
+)
+@Component
+public class TaskMapperImpl implements TaskMapper {
+
+    @Override
+    public TaskResponseDTO toDto(Task entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        TaskResponseDTO taskResponseDTO = new TaskResponseDTO();
+
+        taskResponseDTO.setAssignedUser( entityAssignedUserUserId( entity ) );
+        taskResponseDTO.setTitle( entity.getTitle() );
+        taskResponseDTO.setDescription( entity.getDescription() );
+        taskResponseDTO.setPriority( entity.getPriority() );
+        taskResponseDTO.setCategory( entity.getCategory() );
+        taskResponseDTO.setStatus( entity.getStatus() );
+        taskResponseDTO.setDueDate( entity.getDueDate() );
+
+        return taskResponseDTO;
+    }
+
+    @Override
+    public Task updateTask(TaskRequestDTO updatedTaskDTO, Task entity) {
+        if ( updatedTaskDTO == null ) {
+            return entity;
+        }
+
+        entity.setTitle( updatedTaskDTO.getTitle() );
+        entity.setDescription( updatedTaskDTO.getDescription() );
+        entity.setPriority( updatedTaskDTO.getPriority() );
+        entity.setCategory( updatedTaskDTO.getCategory() );
+        entity.setStatus( updatedTaskDTO.getStatus() );
+        entity.setDueDate( updatedTaskDTO.getDueDate() );
+
+        return entity;
+    }
+
+    @Override
+    public Task toEntity(TaskRequestDTO updatedTaskDTO) {
+        if ( updatedTaskDTO == null ) {
+            return null;
+        }
+
+        Task task = new Task();
+
+        task.setTitle( updatedTaskDTO.getTitle() );
+        task.setDescription( updatedTaskDTO.getDescription() );
+        task.setPriority( updatedTaskDTO.getPriority() );
+        task.setCategory( updatedTaskDTO.getCategory() );
+        task.setStatus( updatedTaskDTO.getStatus() );
+        task.setDueDate( updatedTaskDTO.getDueDate() );
+
+        return task;
+    }
+
+    private Long entityAssignedUserUserId(Task task) {
+        User assignedUser = task.getAssignedUser();
+        if ( assignedUser == null ) {
+            return null;
+        }
+        return assignedUser.getUserId();
+    }
+}
---
+++ TaskTrackerAPI/TaskTrackerAPI/target/generated-sources/annotations/com/nini/TaskTrackerAPI/mapper/UserMapperImpl.java
@@ -0,0 +1,64 @@
+package com.nini.TaskTrackerAPI.mapper;
+
+import com.nini.TaskTrackerAPI.dto.UserRequestDTO;
+import com.nini.TaskTrackerAPI.dto.UserResponseDTO;
+import com.nini.TaskTrackerAPI.model.User;
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2025-11-24T18:12:04+0400",
+    comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.16 (Eclipse Adoptium)"
+)
+@Component
+public class UserMapperImpl implements UserMapper {
+
+    @Override
+    public UserResponseDTO toDto(User entity) {
+        if ( entity == null ) {
+            return null;
+        }
+
+        UserResponseDTO userResponseDTO = new UserResponseDTO();
+
+        userResponseDTO.setFirstName( entity.getFirstName() );
+        userResponseDTO.setLastName( entity.getLastName() );
+        userResponseDTO.setEmail( entity.getEmail() );
+        userResponseDTO.setUsername( entity.getUsername() );
+
+        return userResponseDTO;
+    }
+
+    @Override
+    public User toEntity(UserRequestDTO dto) {
+        if ( dto == null ) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setFirstName( dto.getFirstName() );
+        user.setLastName( dto.getLastName() );
+        user.setEmail( dto.getEmail() );
+        user.setUsername( dto.getUsername() );
+        user.setPassword( dto.getPassword() );
+
+        return user;
+    }
+
+    @Override
+    public User updateEntity(UserRequestDTO dto, User entity) {
+        if ( dto == null ) {
+            return entity;
+        }
+
+        entity.setFirstName( dto.getFirstName() );
+        entity.setLastName( dto.getLastName() );
+        entity.setEmail( dto.getEmail() );
+        entity.setUsername( dto.getUsername() );
+        entity.setPassword( dto.getPassword() );
+
+        return entity;
+    }
+}
