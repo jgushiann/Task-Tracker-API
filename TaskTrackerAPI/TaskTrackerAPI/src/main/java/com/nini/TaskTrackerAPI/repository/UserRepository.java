d92de7f02d8a482c4e22c872f4437d7191ac4d27
jgushiann <njgus21@freeuni.edu.ge>
2025-11-25 18:27:30 +0400

    polished UserRepository class

--- TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/repository/UserRepository.java
+++ TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/repository/UserRepository.java
@@ -1,11 +1,11 @@
 package com.nini.TaskTrackerAPI.repository;
 
-import com.nini.TaskTrackerAPI.model.Task;
 import com.nini.TaskTrackerAPI.model.User;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
-import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
@@ -13,5 +13,18 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     Optional<User> findByUsername(String username);
-    List<User> searchUsers(String firstname, String lastname, String username, String email, Long id);
+
+    @Query("""
+SELECT u FROM User u
+WHERE (:firstname IS NULL OR u.firstName = :firstname)
+AND   (:lastname IS NULL OR u.lastName = :lastname)
+AND   (:username IS NULL OR u.username = :username)
+AND   (:email IS NULL OR u.email = :email)
+AND   (:id IS NULL OR u.userId = :id)
+""")
+    List<User> searchUsers(@Param("firstname") String firstname,
+                           @Param("lastname") String lastname,
+                           @Param("username") String username,
+                           @Param("email") String email,
+                           @Param("id") Long id);
 }
