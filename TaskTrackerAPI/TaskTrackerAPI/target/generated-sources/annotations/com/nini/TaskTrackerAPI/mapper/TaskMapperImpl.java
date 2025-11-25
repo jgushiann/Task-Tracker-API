dd77f6c84220a1dc1c1186c89eeb89616ad2b77e
jgushiann <njgus21@freeuni.edu.ge>
2025-11-25 11:34:13 +0400

    nini

--- TaskTrackerAPI/TaskTrackerAPI/target/generated-sources/annotations/com/nini/TaskTrackerAPI/mapper/TaskMapperImpl.java
+++ TaskTrackerAPI/TaskTrackerAPI/target/generated-sources/annotations/com/nini/TaskTrackerAPI/mapper/TaskMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-25T11:13:57+0400",
+    date = "2025-11-25T11:32:08+0400",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.16 (Eclipse Adoptium)"
 )
 @Component
