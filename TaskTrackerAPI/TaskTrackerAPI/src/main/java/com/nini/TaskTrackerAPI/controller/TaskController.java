8438914944465af2a3848bd8447bf863e45fe258
jgushiann <njgus21@freeuni.edu.ge>
2025-11-25 14:22:03 +0400

    Added @operation annotations to the TaskController class

--- TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/controller/TaskController.java
+++ TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/controller/TaskController.java
@@ -7,6 +7,7 @@ import com.nini.TaskTrackerAPI.mapper.UserMapper;
 import com.nini.TaskTrackerAPI.model.*;
 import com.nini.TaskTrackerAPI.service.TaskService;
 import com.nini.TaskTrackerAPI.service.UserService;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,6 +28,7 @@ public class TaskController {
 
     private final UserService userService;
 
+    @Operation(summary = "Get all/specific tasks")
     @GetMapping
     public ResponseEntity<List<TaskResponseDTO>> searchTasks(@RequestParam(required = false) String title,
                                                             @RequestParam(required = false) String description,
@@ -40,30 +42,35 @@ public class TaskController {
         return ResponseEntity.status(HttpStatus.OK).body(taskDTOs);
     }
 
+    @Operation(summary = "Get task by its ID")
     @GetMapping("/{id}")
     public ResponseEntity<TaskResponseDTO> getTask(@PathVariable("id") Long id){
         TaskResponseDTO taskDTO = taskService.searchTaskById(id);
         return ResponseEntity.status(HttpStatus.OK).body(taskDTO);
     }
 
+    @Operation(summary = "Create a task")
     @PostMapping
     public ResponseEntity<TaskResponseDTO> createTask(@RequestBody @Valid TaskRequestDTO taskDTO){
         TaskResponseDTO createdTask = taskService.createTask(taskDTO);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdTask);
     }
 
+    @Operation(summary = "Update an existing task")
     @PutMapping("/{id}")
     public ResponseEntity<TaskResponseDTO> updateTask(@RequestBody @Valid TaskRequestDTO updatedTaskDTO, @PathVariable Long id){
         TaskResponseDTO updatedTask = taskService.updateTask(updatedTaskDTO, id);
         return ResponseEntity.status(HttpStatus.OK).body(updatedTask);
     }
 
+    @Operation(summary = "Get all the tasks assigned to the user")
     @GetMapping("/user/{user_id}")
     public List<TaskResponseDTO> getTasksForUser(@PathVariable Long user_id){
         User user = userService.searchUserByUserId(user_id);
         return taskService.getTasksByAssignedUser(user);
     }
 
+    @Operation(summary = "Delete a task by its ID")
     @DeleteMapping("/{task_id}")
     public ResponseEntity<Void> deleteTask(@PathVariable Long task_id) {
         taskService.deleteTask(task_id);
