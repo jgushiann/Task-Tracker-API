b47dfdf2862432e8df6906276eb1527954ab9890
jgushiann <njgus21@freeuni.edu.ge>
2025-11-27 12:51:28 +0400

    edited GlobalExceptionHandler class

--- TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/exception/GlobalExceptionHandler.java
+++ TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/exception/GlobalExceptionHandler.java
@@ -1,29 +1,47 @@
 package com.nini.TaskTrackerAPI.exception;
 
 import com.nini.TaskTrackerAPI.dto.ErrorMessageDTO;
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.time.LocalDateTime;
+
 @RestControllerAdvice
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(NotFoundException.class)
-    public ResponseEntity<ErrorMessageDTO> handleNotFound(Exception ex) {
+    public ResponseEntity<ErrorMessageDTO> handleNotFound(Exception ex, HttpServletRequest request) {
         ErrorMessageDTO errorDTO = ErrorMessageDTO.builder()
                 .status(HttpStatus.NOT_FOUND)
                 .message(ex.getMessage())
+                .timestamp(LocalDateTime.now())
+                .path(request.getRequestURI())
                 .build();
         return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDTO);
     }
 
     @ExceptionHandler(AlreadyExistsException.class)
-    public ResponseEntity<ErrorMessageDTO> handleAlreadyExists(Exception ex) {
+    public ResponseEntity<ErrorMessageDTO> handleAlreadyExists(Exception ex, HttpServletRequest request) {
         ErrorMessageDTO errorDTO = ErrorMessageDTO.builder()
                 .status(HttpStatus.CONFLICT)
                 .message(ex.getMessage())
+                .timestamp(LocalDateTime.now())
+                .path(request.getRequestURI())
                 .build();
         return ResponseEntity.status(HttpStatus.CONFLICT).body(errorDTO);
+    }
+
+    @ExceptionHandler(IncorrectCredentialsException.class)
+    public ResponseEntity<ErrorMessageDTO> handleIncorrectCredentials(Exception ex, HttpServletRequest request) {
+        ErrorMessageDTO errorDTO = ErrorMessageDTO.builder()
+                .status(HttpStatus.NOT_FOUND)
+                .message(ex.getMessage())
+                .timestamp(LocalDateTime.now())
+                .path(request.getRequestURI())
+                .build();
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(errorDTO);
     }
 }
