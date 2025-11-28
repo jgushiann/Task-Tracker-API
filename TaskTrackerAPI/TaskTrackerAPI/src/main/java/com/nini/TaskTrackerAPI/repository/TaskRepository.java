4388c77e64975f9ddc7e3e7e97e695907f2f16ff
jgushiann <njgus21@freeuni.edu.ge>
2025-11-28 13:24:04 +0400

    corrected the import in the repository classes

--- TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/repository/TaskRepository.java
+++ TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/repository/TaskRepository.java
@@ -1,7 +1,8 @@
 package com.nini.TaskTrackerAPI.repository;
 
 import com.nini.TaskTrackerAPI.model.*;
-import org.apache.ibatis.annotations.Param;
+import org.springframework.data.repository.query.Param;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
--- TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/repository/UserRepository.java
+++ TaskTrackerAPI/TaskTrackerAPI/src/main/java/com/nini/TaskTrackerAPI/repository/UserRepository.java
@@ -1,7 +1,7 @@
 package com.nini.TaskTrackerAPI.repository;
 
 import com.nini.TaskTrackerAPI.model.User;
-import org.apache.ibatis.annotations.Param;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
