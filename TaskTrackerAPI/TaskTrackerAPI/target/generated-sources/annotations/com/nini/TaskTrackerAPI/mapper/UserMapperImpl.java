61a8d8e8e87b03b139506b44f6911897e1aa9b93
jgushiann <njgus21@freeuni.edu.ge>
2025-12-03 12:28:40 +0400

    fixing

--- TaskTrackerAPI/TaskTrackerAPI/target/generated-sources/annotations/com/nini/TaskTrackerAPI/mapper/UserMapperImpl.java
+++ TaskTrackerAPI/TaskTrackerAPI/target/generated-sources/annotations/com/nini/TaskTrackerAPI/mapper/UserMapperImpl.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-28T14:58:03+0400",
+    date = "2025-12-03T12:22:03+0400",
     comments = "version: 1.6.3, compiler: javac, environment: Java 17.0.16 (Eclipse Adoptium)"
 )
 @Component
